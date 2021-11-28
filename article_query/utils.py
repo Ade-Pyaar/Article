5b40f616d10a0499027f71c531bddf5102394858
Ibrahim Adebayo <Adebayoibrahim2468@gmail.com>
2021-11-28 14:42:26 +0100

    I hate js

--- article_query/utils.py
+++ article_query/utils.py
@@ -1,7 +1,6 @@
 from .models import Articles
 import requests, json
 from bs4 import BeautifulSoup
-from sqlalchemy.exc import IntegrityError
 
 
 def store_articles(db):
@@ -16,19 +15,17 @@ def store_articles(db):
         persons = ','.join(total_data[key]['persons'])
         keywords = ','.join(total_data[key]['keywords'])
 
-        try:
-            new_article = Articles(
-                title = title,
-                link = article_link,
-                date = pub_date,
-                persons = persons,
-                keywords = keywords,
-            )
+        new_article = Articles(
+            title = title,
+            link = article_link,
+            date = pub_date,
+            persons = persons,
+            keywords = keywords,
+        )
 
-            db.session.add(new_article)
-            db.session.commit()
-        except IntegrityError:
-            db.session.rollback()
+        db.session.add(new_article)
+
+    db.session.commit()
 
 
 
