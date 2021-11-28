4fd2e0b64b0b4ec63c89d340add606ce8b52ef05
Ibrahim Adebayo <Adebayoibrahim2468@gmail.com>
2021-11-28 14:58:18 +0100

    Update routes.py

--- article_query/routes.py
+++ article_query/routes.py
@@ -10,12 +10,12 @@ from .utils import store_articles, scrape_articles
 
 
 if not os.path.exists("article_query/article.db"):
-    try:
+
         
-        db.create_all()
-        store_articles(db)
-     except:
-        pass
+    db.create_all()
+    store_articles(db)
+
+
 
 
 
