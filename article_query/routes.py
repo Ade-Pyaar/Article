1d33adc3550706968e0f43681a5311162c755213
Ibrahim Adebayo <Adebayoibrahim2468@gmail.com>
2021-11-29 08:52:53 +0100

    seriously

--- article_query/routes.py
+++ article_query/routes.py
@@ -1,21 +1,13 @@
 from flask import render_template, request, flash, url_for
-from article_query import app, db
-from .models import Articles
-import os
-from .utils import store_articles, scrape_articles
+from article_query import app
+import json
+
+from .utils import scrape_articles
 
 
 
-
-
-
-if not os.path.exists("article_query/article.db"):
-
-        
-    db.create_all()
-    store_articles(db)
-
-
+with open('total.json', 'r') as file:
+    total = json.load(file)
 
 
 
@@ -24,20 +16,38 @@ if not os.path.exists("article_query/article.db"):
 def home():
     page_content = 'form'
     result = []
+    people_links = {}
     if request.method == 'POST':
         page_content = 'result'
         keyword = request.form.get('keyword').strip().lower()
 
-        all_articles = Articles.query.order_by(Articles.title)
-        for item in all_articles:
-            if keyword in item.title.lower():
-                result.append(item)
+        if keyword == '' or keyword == ' ':
+            page_content = 'form'
+            flash("Empty keyword is not allowed", 'danger')
+            return render_template('home.html', title='Article Query', page_content=page_content, result=result)
+
+        else:
+
+            # search the titles for the user keyword
+            for key in total.keys():
+                if keyword in total[key]['title'].lower():
+                    result.append(total[key])
+
+            # search the keywords for the user keyword
+            for key in total.keys():
+                if keyword in ','.join(total[key]['keywords']).lower():
+                    result.append(total[key])
+
 
         if len(result) < 1:
             page_content = 'form'
             flash("No result found for you keyword, try another keyword", 'danger')
+
     
     return render_template('home.html', title='Article Query', page_content=page_content, result=result)
+
+
+
 
 
 
@@ -45,6 +55,5 @@ def home():
 @app.route('/scrape')
 def scrape():
     scrape_articles()
-    store_articles(db)
 
     return url_for('home')
--- article_query/utils.py
+++ article_query/utils.py
@@ -1,31 +1,6 @@
-from .models import Articles
 import requests, json
 from bs4 import BeautifulSoup
 
-
-def store_articles(db):
-    with open('total.json', 'r') as file:
-        total_data = json.load(file)
-    
-    
-    for key in total_data.keys():
-        title = total_data[key]['title']
-        article_link = total_data[key]['article_link']
-        pub_date = total_data[key]['pub_link']
-        persons = ','.join(total_data[key]['persons'])
-        keywords = ','.join(total_data[key]['keywords'])
-
-        new_article = Articles(
-            title = title,
-            link = article_link,
-            date = pub_date,
-            persons = persons,
-            keywords = keywords,
-        )
-
-        db.session.add(new_article)
-
-    db.session.commit()
 
 
 
@@ -41,6 +16,7 @@ def scrape_articles():
         html = BeautifulSoup(data.text, 'html.parser')
 
         articles = html.select('div.result-container')
+
 
         for i in range(len(articles)):
 
@@ -58,10 +34,10 @@ def scrape_articles():
                 'title': title,
                 'article_link': article_link,
                 'pub_link': pub_date,
-                'persons': [person.get_text() for person in articles[i].find_all(attrs={"class": "link person"})],
+                'persons': {person.get_text(): person.get('href') for person in articles[i].find_all(attrs={"class": "link person"})},            
                 'keywords': [word.get_text() for word in single_html.find_all(attrs={'class': 'userdefined-keyword'})]
             }
 
+
     with open('total.json', 'w') as file:
         json.dump(total, file)
-
